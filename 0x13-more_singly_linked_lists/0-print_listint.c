ac37a59a703cdfa7d108747b5da70eead1e7ca7d
emanahmed-ux <ema.newgitl7@gmail.com>
2023-04-03 17:20:22 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count = 0;
 
-	for (; h != NULL; h = h->next)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
+		count++;
+		h = h->next;
 	}
-
-	return (num);
+	return (count);
 }
