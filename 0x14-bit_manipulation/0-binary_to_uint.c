e455014b3798df88313c232dfaea7ea5eaa6ea36
emanahmed-ux <ema.newgitl7@gmail.com>
2023-04-09 11:59:50 -0700

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,32 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 	unsigned int result = 0;
 
-	while (*b != '\0')
+	while (*b != '\0)
+
 	{
-		result = result * 2 + (*b - '0');
+		if (*b == '0')
+		{
+			result <<= 1;
+
+		}
+		else if (*b == '1')
+		{
+			result = (result << 1) | 1;
+
+		}
+		else
+		{
+			return (0);
+		}
 		b++;
 	}
+
 	return (result);
 }
