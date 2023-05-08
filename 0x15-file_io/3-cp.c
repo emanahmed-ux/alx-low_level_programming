9274b6a6ba049c12202f4bea1a2857561b7cd9e0
emanahmed-ux <ema.newgirl7@gmail.com>
2023-05-08 07:09:04 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include "file.h"
+/**
+ * main - Copies the contents of a file to another file.
+ * @my_argc: number of arguments.
+ * @my_argv: An array of pointers.
+ * Return: 0 on success.
+ * Description: incorrect - exit code 97, not found or read - exit code 98.
+ *              cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int my_argc, char *my_argv[])
+{
+	int from_fil, to_fil, m, s;
+	char buffer[BUFFER_SIZE];
+
+	if (my_argc != 3)
+	{
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from_fil = open(my_argv[1], O_RDONLY);
+
+	if (from_fil == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", my_argv[1]);
+		exit(98);
+	}
+	to_fil = open(my_argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	if (to_fil == -1)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", my_argv[2]);
+		close(from_fil);
+		exit(99);
+	}
+	while ((m = read(from_fil, buffer, BUFFER_SIZE)) > 0)
+	{
+		s = write(to_fil, buffer, m);
+
+		if (s == -1 || s != m)
+		{
+			fprintf(stderr, "Error: Can't write to %s\n", my_argv[2]);
+			close(from_fil);
+			close(to_fil);
+			exit(99);
+		}
+	}
+	if (m == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", my_argv[1]);
+		close(from_fil);
+		close(to_fil);
+		exit(98);
+	}
+	if (close(from_fil) == -1)
+	{
+		fprintf(stderr, "Error: Can't close file descriptor %d\n", from_fil);
+		exit(100);
+	}
+	if (close(to_fil) == -1)
+	{
+		fprintf(stderr, "Error: Can't close file descriptor %d\n", to_fil);
+		exit(100);
+	}
+	return (0);
+}
