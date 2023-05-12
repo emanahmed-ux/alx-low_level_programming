e12233d50cb5b639f060c25acc8a2782b6443136
emanahmed-ux <ema.newgirl7@gmail.com>
2023-05-12 12:34:25 -0700

    try a new thougth

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,24 +1,4 @@
-#include <elf.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#define ID 16
-void ver_sion(unsigned char *e_ident);
-void clo_sing(int file_elf);
-int main(int dtr, char *ker[]);
-void class(unsigned char *e_ident);
-int elf_che(unsigned char *e_ident);
-void magic(unsigned char *e_ident);
-void osabi(unsigned char *e_ident);
-void abi(unsigned char *e_ident);
-void type(unsigned int e_type, unsigned char *e_ident);
-void entry(unsigned long int e_entry, unsigned char *e_ident);
-void data(unsigned char *e_ident);
-int main(int argc, char **argv);
+#include "elf_a.h"
 /**
  * ver_sion - shows the version of the elf
  * @e_ident: pointers of arrays
